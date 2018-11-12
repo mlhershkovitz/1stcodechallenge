ba9d760815eafa4187f2509fc63a355b5b14714d
Madison Hershkovitz <mlhersh6@gmail.com>
2018-11-12 14:31:50 -0600

    working on swap button

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
-$( document ).ready(readyNow);
+$(document).ready(readyNow);
 
 function readyNow() {
     $('#generateButton').on('click', generateButton);
+    $('#swapButton').on('click', swapButton);
+    //$('#deleteButton').on('click', deleteButton);
+
 }
 
 let generateCount = 0; //creating the variable that counts the clicks
@@ -9,7 +12,7 @@ let generateCount = 0; //creating the variable that counts the clicks
 
 function generateButton() {
     generateCount++; //so our count increments by 1
-console.log(generateCount);
+    console.log(generateCount);
 
     let element = $('.rectangle'); //so js knows what element to append to the dom
     element.append(`<div class="generate">
@@ -18,3 +21,11 @@ console.log(generateCount);
     <button class="deleteButton">Delete</button>    
     </div>`);
 }
+
+//was working on functionality for this part
+
+function swapButton() {
+    $(this).parent().toggleClass('generate2');
+}
+
+//then would have done almost the same for delete
