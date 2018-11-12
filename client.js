280bef8c5ccb9f0b3e4cc9e42e7df063e385a77e
Madison Hershkovitz <mlhersh6@gmail.com>
2018-11-12 13:59:41 -0600

    appends a div to click and shows how many clicks

--- client.js
+++ client.js
@@ -1,9 +1,18 @@
 $( document ).ready(readyNow);
 
 function readyNow() {
-    $('#generate').on('click', generateButton);
+    $('#generateButton').on('click', generateButton);
 }
 
+let generateCount = 0; //creating the variable that counts the clicks
+// ^this must be outside of the function
+
 function generateButton() {
+    generateCount++; //so our count increments by 1
+
+    let element = $('.rectangle'); //so js knows what element to append to the dom
+    element.append(`<div class="generate">
+    <p>${generateCount}</p>
     
+    </div>`);
 }
