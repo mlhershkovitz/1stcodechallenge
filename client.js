bacb6037aaa05b681751dd882ff652c2716e3e45
Madison Hershkovitz <mlhersh6@gmail.com>
2018-11-12 14:06:24 -0600

    create swap and delete buttons

--- client.js
+++ client.js
@@ -9,10 +9,12 @@ let generateCount = 0; //creating the variable that counts the clicks
 
 function generateButton() {
     generateCount++; //so our count increments by 1
+console.log(generateCount);
 
     let element = $('.rectangle'); //so js knows what element to append to the dom
     element.append(`<div class="generate">
     <p>${generateCount}</p>
-    
+    <button id="swapButton">Swap</button> 
+    <button id="deleteButton">Delete</button>    
     </div>`);
 }
